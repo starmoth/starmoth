4ca2a5f420180656e0e142525016b82e15edf6cd
Andrew Copland <andrewjohncopland@yahoo.co.uk>
2014-05-18 20:27:09 +0100

    Disable drawing the point stars since it's broken.

--- src/Background.cpp
+++ src/Background.cpp
@@ -356,7 +356,7 @@ Container::Container(Graphics::Renderer *renderer, Random &rand)
 , m_milkyWay(renderer)
 , m_starField(renderer, rand)
 , m_universeBox(renderer)
-, m_drawFlags( DRAW_SKYBOX | DRAW_STARS )
+, m_drawFlags( DRAW_SKYBOX /*| DRAW_STARS*/ )
 {
 	Graphics::RenderStateDesc rsd;
 	rsd.depthTest  = false;
